bcb4a723a511436938e14b62386756e56c4c47cc
Brian Hines <brian@projectweekend.net>
2015-06-08 21:42:10 -0500

    Update validation middleware

--- app/middleware/validation.py
+++ app/middleware/validation.py
@@ -1,5 +1,7 @@
 class RequestValidation(object):
 
     def process_resource(self, req, res, resource):
-        if hasattr(resource, 'validate'):
-            resource.validate(req.context['data'])
+        if req.method == 'POST':
+            resource.validate_create(req.context['data'])
+        if req.method == 'PUT':
+            resource.validate_update(req.context['data'])
