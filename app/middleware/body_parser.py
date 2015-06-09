4b144c2bcc3da1003da41c0c7507f5e721fdbb30
Brian Hines <brian@projectweekend.net>
2015-06-08 21:41:57 -0500

    Update body parser

--- app/middleware/body_parser.py
+++ app/middleware/body_parser.py
@@ -6,11 +6,12 @@ class JSONBody(object):
 
     def process_request(self, req, res):
         if req.content_type == 'application/json':
-            try:
-                req.context['data'] = json.loads(req.stream.read())
-            except ValueError:
-                message = "Request body is not valid 'application/json'"
-                raise falcon.HTTPBadRequest('Bad request', message)
+            if req.method in ['POST', 'PUT']:
+                try:
+                    req.context['data'] = json.loads(req.stream.read())
+                except ValueError:
+                    message = "Request body is not valid 'application/json'"
+                    raise falcon.HTTPBadRequest('Bad request', message)
         else:
             message = "Content-Type must be 'application/json'"
             raise falcon.HTTPBadRequest('Bad request', message)
