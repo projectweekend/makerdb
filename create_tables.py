1d1e0124f6cf93d25dad2e0a1af9a6de3ea6117f
Brian Hines <brian@projectweekend.net>
2015-06-13 18:23:25 -0500

    Add create DynamoDB tables script

---
+++ create_tables.py
@@ -0,0 +1,35 @@
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.fields import HashKey, RangeKey
+from boto.dynamodb2.types import STRING
+from boto.exception import JSONResponseError
+
+
+def create_makerdb_users():
+    Table.create(
+        'makerdb_users',
+        schema=[
+            HashKey('email', data_type=STRING),
+            RangeKey('account_type', data_type=STRING)
+        ],
+        throughput={
+            'read': 1,
+            'write': 1
+        }
+    )
+
+
+def main():
+
+    tasks = [
+        create_makerdb_users
+    ]
+
+    for task in tasks:
+        try:
+            task()
+        except JSONResponseError as e:
+            print(e.message)
+
+
+if __name__ == '__main__':
+    main()
