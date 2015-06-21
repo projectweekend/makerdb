37e878fc4a38dd467180b6f3b9dea4730d459563
Brian Hines <brian@projectweekend.net>
2015-06-21 01:57:47 -0500

    Create inventory table

---
+++ sql/V4__Create_app_inventory_items_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS "app_inventory_items"
+(
+    id              SERIAL PRIMARY KEY,
+    user_id         INTEGER REFERENCES app_users(id),
+    name            TEXT,
+    url             TEXT,
+    image_url       TEXT,
+    quantity        INTEGER,
+    vendor_name     TEXT,
+    vendor_item_id  TEXT,
+    vendor_site     TEXT
+);
+
+CREATE INDEX        idx_inventory_item_name
+ON                  app_inventory_items
+USING               btree(name text_pattern_ops);
